18bb0f0947686821591c4c713b86e82a0c38f267
John-Paul Robinson <jprorama@gmail.com>
2017-04-21 12:13:19 -0500

    Initial config file parser
    Provides a generator that produces records from the config file
    Uses a simple printer to demonstrate functionality

---
+++ s2l.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+#
+# parse a sympa config file
+#
+
+import fileinput
+import re
+
+# record generator for sympa config file
+# each record is separated by blank lines
+# records are returned one record at a time
+def record(file):
+    currec = ""
+    found = False # skip leading blank lines in file
+    for line in file:
+        line = line.rstrip("\n")
+        if re.search("^$", line):
+            if found:
+                yield currec
+                currec = ""
+            else:
+                next
+        else:
+            found = True
+            if currec:
+                currec = currec + "\n" + line
+            else:
+                currec = line
+
+# process config file one record at a time
+i=0
+for rec in record(fileinput.input()):
+    i = i + 1
+    print "record", i
+    print rec
+    print
